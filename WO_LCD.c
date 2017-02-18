d1e8b57518d1e83c8afded65031bffe71d0d18de
aleksandra-s <aleksandra.soltan@gmail.com>
2017-02-18 15:41:00 +0100

    Update WO_LCD.c

--- WO_LCD.c
+++ WO_LCD.c
@@ -2,12 +2,12 @@
 void navigate_cursor_up(){
   textbuffer[currentline][12] = ' ';
   currentline = mod((currentline + 1),4);
-	textbuffer[currentline][12] = '*';
+  textbuffer[currentline][12] = '*';
 }
 
 // Navigate menu cursor (*) down a line
 void navigate_cursor_down(){
   textbuffer[currentline][12] = ' ';
-	currentline = mod((currentline - 1),4);
-	textbuffer[currentline][12] = '*';
+  currentline = mod((currentline - 1),4);
+  textbuffer[currentline][12] = '*';
 }
