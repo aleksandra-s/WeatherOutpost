ae750092f36d320440f6ff729012abe9febc311e
ReneDescarnage <Kyle.dean.sinclair@gmail.com>
2017-02-20 14:55:33 +0100

    Update aux_functions.c

--- aux_functions.c
+++ aux_functions.c
@@ -2,12 +2,26 @@
 //These include things like the mod function, whose sole purpose is
 //to allow modular arithmetic to work with negative numbers
 
-void initialize_LED(){
-	volatile int* trise = (volatile int*) 0xbf886100; 
-    *trise = *trise & ~0xff; // set LEDs to output (initialize 8 lsb of Port E to outputs)
-	//volatile int* porte = (volatile int*) 0xbf886110;
-	*porte = *porte | 0x0; // LEDs begin by showing value 0 (initialize value of Port E to 0) 
-}
+void labinit( void )
+	{
+  TRISE = TRISE & ~0xff; // set LEDs to output (initialize 8 lsb of Port E to outputs)
+  TRISD = TRISD | ~0xffffe01f; // set bits 11 through 5 of Port D as inputs
+
+
+  T2CON = 0x70; // set prescale for timer to 1:256
+  TMR2 = 0x0; // Clear timer register
+  PR2 = 31250; // Load period register (80 mHz/256/10 = 31,250)
+
+  IPC(2) = IPC(2) | 0x1C; // set highest priority (7)
+  IPC(2) = IPC(2) | 0x3; // set highest subpriority (3)
+  enable_interrupt(); // enable global interrupts
+  IEC(0) = IEC(0) | 0x100; // enable Timer 2 interrupt
+
+  T2CON = T2CON | 0x8000; // Start timer
+	display_menu(0); //Displays initial options
+	textbuffer[0][12] = '*';
+	}
+
 
 
 
