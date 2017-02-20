099be8efdb80cad42d3698cd097d56c5e8f1e87c
ReneDescarnage <Kyle.dean.sinclair@gmail.com>
2017-02-20 14:29:17 +0100

    Update WO_menu_navigation.c

--- WO_menu_navigation.c
+++ WO_menu_navigation.c
@@ -1,56 +1,123 @@
 
 #include <stdint.h>   /* Declarations of uint_32 and the like */
 #include <pic32mx.h>  /* Declarations of system-specific addresses etc */
-#include "mipslab.h"  /* Declarations for these labs */
+#include "master.h"  /* Declarations for all functions and data */
+
+
+
+
 void navigate(volatile int *currentline,volatile int *currentmenu)
 	{
-    textbuffer[*currentline][12] = ' ';
-		if(*currentmenu == 0)
-			{
-      select_line(currentline, currentmenu);
-			}
-		else if(*currentmenu != 0)
-			{
-			(*currentmenu)= 0;
-			display_menu(1);
-		  }
 
-	*currentline = 0;
-  PORTE = PORTE + *currentmenu;
-	textbuffer[*currentline][12] = '*';
+
+	if(*currentmenu == 0)
+		{
+    select_line(*currentline, currentmenu);
+		}
+
+	else if(*currentmenu == 1) //This is the cities screen. Index line 4 is back. All else calls ESP
+		{
+		select_line(*currentline + 4, currentmenu);
+	  }
+
+	else if(*currentmenu == 2)
+		{
+		select_line(*currentline + 8, currentmenu);
+		}
+
+	PORTE = *currentmenu;
+	clear_line(currentline);
   return;
 	}
 
 
-void select_line(volatile int *currentline, volatile int *currentmenu)
+void select_line(int currentline, volatile int *currentmenu)
   {
-  switch(*currentline)
+  switch(currentline)
     {
+
+			// home menu options *************************************
     case 0:
       {
-      display_menu(2);//menu 2 is the list of cities to choose from. It is menu 2
-      *currentmenu = 2;
+      display_menu(1);//menu 1 is the list of cities to choose from.
+      *currentmenu = 1;
       return;
       }
     case 1:
       {
-      *currentmenu = 3;
-      display_menu(3);//This is the wifi options menu. It is menu case 3
+      *currentmenu = 2;
+      display_menu(2);//This is the wifi options menu.
       return;
       }
     case 2:
-      display_string(0,"Not implemented yet");
-      quicksleep(1000);
+			{
       *currentmenu = 0;
-      display_menu(1);
+      display_menu(0);
       return;
+			}
     case 3:
       {
-      display_string(0,"Not implemented yet");
-      quicksleep(1000);
       *currentmenu = 0;
-      display_menu(1);
+      display_menu(0);
       return;
       }
+
+
+
+			// City menu options ************************************
+		case 4:
+			{
+				display_menu(1);//Unimplemented
+				*currentmenu = 1;
+				return;
+			}
+		 case 5:
+		 {
+			display_menu(1);//Unimplemented
+ 			*currentmenu = 1;
+  	  return;
+
+ 		 }
+		 case 6:
+		 {
+			display_menu(1);//Unimplemented
+			*currentmenu = 1;
+ 			return;
+ 		 }
+		 case 7:
+		 {
+			display_menu(0);//back function
+			*currentmenu = 0;
+			return;
+		 }
+
+
+		 
+		 // Wifi menu options **************************************
+		 case 8:
+		 {
+		  display_menu(2);//Unimplemented
+ 		  *currentmenu = 2;
+ 			return;
+ 		 }
+		 case 9:
+		 {
+			 display_menu(2);//Unimplemented
+			 *currentmenu = 2;
+ 			return;
+ 		 }
+		 case 10:
+		 {
+			 display_menu(2);//Unimplemented
+			 *currentmenu = 2;
+			return;
+		 }
+		 case 11:
+		 {
+			display_menu(0);//back function
+			*currentmenu = 0;
+			return;
+		 }
+
     }
   }
